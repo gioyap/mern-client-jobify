044e014480db32275cb0b7f545d1d51a29b7814b
Gio Yap <gioedrian.yap.l@gmail.com>
2024-04-17 23:47:50 +0800

    express middleware for validation

---
+++ middleware/validationMiddleware.js
@@ -0,0 +1,58 @@
+import { body, validationResult, param } from 'express-validator';
+import { BadRequestError, NotFoundError } from '../errors/customErrors';
+import { JOB_STATUS, JOB_TYPE } from '../utils/constants.js';
+import { validateJobInput } from '../middleware/validationMiddleware.js';
+import mongoose from 'mongoose';
+import Job from '../models/JobModel.js';
+
+const withValidationErrors = (validateValues) => {
+    return [
+      validateValues,
+      (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          const errorMessages = errors.array().map((error) => error.msg);
+          if (errorMessages[0].startsWith('no job')) {
+            throw new NotFoundError(errorMessages);
+          }
+          throw new BadRequestError(errorMessages);
+        }
+        next();
+      },
+    ];
+  };
+
+// export const validateTest = withValidationErrors([
+//   body('name')
+//     .notEmpty()
+//     .withMessage('name is required')
+//     .isLength({ min: 3, max: 50 })
+//     .withMessage('name must be between 3 and 50 characters long')
+//     .trim(),
+// ]);
+
+export const validateJobInput = withValidationErrors([
+    body('company').notEmpty().withMessage('company is required'),
+    body('position').notEmpty().withMessage('position is required'),
+    body('jobLocation').notEmpty().withMessage('job location is required'),
+    body('jobStatus')
+      .isIn(Object.values(JOB_STATUS))
+      .withMessage('invalid status value'),
+    body('jobType').isIn(Object.values(JOB_TYPE)).withMessage('invalid job type'),
+  ]);
+
+  router.route('/').get(getAllJobs).post(validateJobInput, createJob);
+router
+  .route('/:id')
+  .get(getJob)
+  .patch(validateJobInput, updateJob)
+  .delete(deleteJob);
+
+  export const validateIdParam = withValidationErrors([
+    param('id').custom(async (value) => {
+      const isValidId = mongoose.Types.ObjectId.isValid(value);
+      if (!isValidId) throw new BadRequestError('invalid MongoDB id');
+      const job = await Job.findById(value);
+      if (!job) throw new NotFoundError(`no job with id : ${value}`);
+    }),
+  ]);
